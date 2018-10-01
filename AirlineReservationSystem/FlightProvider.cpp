91b8b72b50c71db4f478087635a1a16c17965cd9
yukbu406 <42500041+yukbu406@users.noreply.github.com>
2018-09-30 22:11:47 -0700

    changed menu option/ realigned flight schedule

--- AirlineReservationSystem/FlightProvider.cpp
+++ AirlineReservationSystem/FlightProvider.cpp
@@ -20,12 +20,12 @@ FlightProvider::~FlightProvider()
 
 std::vector<Flight*> FlightProvider::GetFlights()
 {
-	Flight* sea = new Flight(1, "       SEA,WA  ", "    ORD,IL  ", "     3:00PM", "    5:00PM        ",200);
-	Flight* ord = new Flight(2, "       ORD,IL  ", "    SEA,WA  ", "     3:00PM", "    5:00PM        ",200);
-	Flight* lax = new Flight(3, "       LAX,WA  ", "    SEA,WA  ", "     3:00PM", "    5:00PM        ",200);
-	Flight* iah = new Flight(4, "       IAH,TX  ", "    SEA,WA  ", "     3:00PM", "    5:00PM        ",200);
-	Flight* jfk = new Flight(5, "       JFG,NY  ", "    SEA,WA  ", "     3:00PM", "    5:00PM        ",200);
-	Flight* stl = new Flight(6, "       STL,MO  ", "    SEA,WA  ", "     3:00PM", "    5:00PM        ",200);
+	Flight* sea = new Flight(1, "       SEA,WA  ", "    ORD,IL  ", "     3:00PM", "    5:00PM         ",200);
+	Flight* ord = new Flight(2, "       ORD,IL  ", "    SEA,WA  ", "     3:00PM", "    5:00PM         ",200);
+	Flight* lax = new Flight(3, "       LAX,WA  ", "    SEA,WA  ", "     3:00PM", "    5:00PM         ",200);
+	Flight* iah = new Flight(4, "       IAH,TX  ", "    SEA,WA  ", "     3:00PM", "    5:00PM         ",200);
+	Flight* jfk = new Flight(5, "       JFG,NY  ", "    SEA,WA  ", "     3:00PM", "    5:00PM         ",200);
+	Flight* stl = new Flight(6, "       STL,MO  ", "    SEA,WA  ", "     3:00PM", "    5:00PM         ",200);
 
 
 	std::vector<Flight*> FlightDetail = { sea, ord, lax, iah, jfk, stl };
